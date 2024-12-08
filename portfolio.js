fe4798530e675c921fbfb7f269728eca22f62d8d
Obilana Eniola <obilanaeniola01@gmail.com>
2024-12-08 13:34:11 +0100

    updated

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,16 @@
-let menuicon = document.querySelector('menu-icon');
+document.addEventListener('DOMContentLoaded', ()=>{
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+console.log('menuIcon:', menuIcon);
+
+if(menuIcon) {
+
 menuIcon.onclick = () => {
-    menuicon.classList.toggle('bx-x')
+    menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active');
+};
+} else {
+    console.error('MenuIcon not found');
 }
+});
