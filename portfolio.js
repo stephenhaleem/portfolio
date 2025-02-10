b8c8518fce60f31b59574d7a975e1f76421de2c4
Obilana Eniola <Obilanaeniola01@gmail.com>
2025-02-10 14:58:03 +0100

    added stuff

--- portfolio.js
+++ portfolio.js
@@ -1,39 +1,25 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
-console.log('menuIcon:', menuIcon);
-
-if(menuIcon) {
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active');
-};
-} else {
-    console.error('MenuIcon not found');
-}
-});document.addEventListener('DOMContentLoaded', ()=>{
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-console.log('menuIcon:', menuIcon);
-
-if(menuIcon) {
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active');
-};
-} else {
-    console.error('MenuIcon not found');
-}
-});
+      if(navToggle){
+        navToggle.addEventListener('click', () =>{
+           navMenu.classList.add('show-menu')
+        })
+     }
+     
+     /* Menu hidden */
+     if(navClose){
+        navClose.addEventListener('click', () =>{
+           navMenu.classList.remove('show-menu')
+        })
+     }
+     
 
 var loader = document.getElementById("pre-loader");
 
 window.addEventListener("load", function() {
     setTimeout(function() {
         loader.style.display = "none";
-    }, 1500); // Change this time to delay the loading screen. 1000 milliseconds = 1 second. 5000 milliseconds = 5 seconds. 10000 milliseconds = 10 seconds. ect. 1000 milliseconds = 1 second. 5000 milliseconds = 5 seconds. 10000 milliseconds = 10 seconds. ect.
+    }, 2000); // Change this time to delay the loading screen. 1000 milliseconds = 1 second. 5000 milliseconds = 5 seconds. 10000 milliseconds = 10 seconds. ect. 1000 milliseconds = 1 second. 5000 milliseconds = 5 seconds. 10000 milliseconds = 10 seconds. ect.
 });
