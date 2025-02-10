fc7e7b2b81c9b635e859d1c0320d3dff6fb33f53
Obilana Eniola <Obilanaeniola01@gmail.com>
2025-02-10 14:38:57 +0100

    updated

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 let menuIcon = document.querySelector('#menu-icon');
-document.addEventListener('DOMContentLoaded', ()=>{
+let navbar = document.querySelector('.navbar');
+
+console.log('menuIcon:', menuIcon);
+
+if(menuIcon) {
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active');
+};
+} else {
+    console.error('MenuIcon not found');
+}
+});document.addEventListener('DOMContentLoaded', ()=>{
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -9,6 +22,18 @@ console.log('menuIcon:', menuIcon);
 if(menuIcon) {
 
 menuIcon.onclick = () => {
-    menuicon.classList.toggle('bx-x')
+    menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active');
+};
+} else {
+    console.error('MenuIcon not found');
 }
+});
+
+var loader = document.getElementById("pre-loader");
+
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        loader.style.display = "none";
+    }, 1500); // Change this time to delay the loading screen. 1000 milliseconds = 1 second. 5000 milliseconds = 5 seconds. 10000 milliseconds = 10 seconds. ect. 1000 milliseconds = 1 second. 5000 milliseconds = 5 seconds. 10000 milliseconds = 10 seconds. ect.
+});
